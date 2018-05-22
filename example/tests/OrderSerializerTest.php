916ae032fbd338cbb5ebd54be7023bfcfb4d71be
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-05-22 10:27:02 +0200

    Clean up & update example

--- example/tests/OrderSerializerTest.php
+++ example/tests/OrderSerializerTest.php
@@ -21,7 +21,5 @@ class OrderSerializerTest extends TestCase
         ]);
 
         $this->assertMatchesJsonSnapshot($orderSerializer->serialize($order));
-        $this->assertMatchesJsonSnapshot($orderSerializer->serialize($order));
-        $this->assertMatchesJsonSnapshot($orderSerializer->serialize($order));
     }
 }
