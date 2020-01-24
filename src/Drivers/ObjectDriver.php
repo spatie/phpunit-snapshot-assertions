6a47e43962bb07a2db6df26b8625c9d22b73ee84
Nicolas Clavaud <nicolas.clavaud@manomano.com>
2020-01-24 17:25:09 +0100

    Add ObjectDriver
    fixes https://github.com/spatie/phpunit-snapshot-assertions/issues/46
    
    Add an ObjectDriver that relies on Symfony Serializer and will snapshot
    public properties and the return of public getters into readable JSON.
    This was motivated by offering a more convenient alternative to the
    current VarDriver.

---
+++ src/Drivers/ObjectDriver.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Spatie\Snapshots\Drivers;
+
+use PHPUnit\Framework\Assert;
+use Spatie\Snapshots\Driver;
+use Symfony\Component\Serializer\Encoder\JsonEncode;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+class ObjectDriver implements Driver
+{
+    public function serialize($data): string
+    {
+        $normalizers = [
+            new DateTimeNormalizer(),
+            new ObjectNormalizer(),
+        ];
+
+        $encoders = [
+            new JsonEncoder(),
+        ];
+
+        $serializer = new Serializer($normalizers, $encoders);
+
+        return $serializer->serialize($data, 'json', [
+            JsonEncode::OPTIONS => JSON_PRETTY_PRINT,
+        ]);
+    }
+
+    public function extension(): string
+    {
+        return 'json';
+    }
+
+    public function match($expected, $actual)
+    {
+        Assert::assertJsonStringEqualsJsonString($expected, $this->serialize($actual));
+    }
+}
---
+++ tests/Unit/Drivers/ObjectDriverTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Spatie\Snapshots\Test\Unit\Drivers;
+
+use PHPUnit\Framework\TestCase;
+use Spatie\Snapshots\Drivers\ObjectDriver;
+
+class ObjectDriverTest extends TestCase
+{
+    /** @test */
+    public function it_can_serialize_an_object()
+    {
+        $driver = new ObjectDriver();
+
+        $expected = <<<JSON
+{
+    "name": "My name",
+    "valid": true,
+    "dateTime": "2020-01-01T15:00:00+01:00",
+    "public": "public"
+}
+JSON;
+
+        $this->assertEquals($expected, $driver->serialize(new Obj()));
+    }
+}
+
+class Obj
+{
+    private $private = 'private';
+    public $public = 'public';
+
+    public function getName()
+    {
+        return 'My name';
+    }
+
+    public function isValid()
+    {
+        return true;
+    }
+
+    public function getDateTime()
+    {
+        return new \DateTimeImmutable('2020-01-01 15:00:00+01:00');
+    }
+}
